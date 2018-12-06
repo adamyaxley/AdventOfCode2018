190df91ec6bc10bb7fc249835442d921718a36ad
Adam Yaxley <craincat666@gmail.com>
2018-12-06 15:49:33 +0900

    Day 1 complete

--- day1.cpp
+++ day1.cpp
@@ -1,7 +1,26 @@
 #include "gtest/gtest.h"
 #include "util.h"
 
+#include <numeric>
+
 TEST(Problems, Day1)
 {
-	ASSERT_TRUE(true);
+	auto input = get_input("../../Input/day1.txt");
+	auto data = string_split<int64>(input, "\n");
+	auto result = std::accumulate(data.begin(), data.end(), (int64)0);
+
+	// Part 1
+	ASSERT_EQ(590, result);
+
+	std::set<int64> frequencyReached;
+	int64 currentFrequency = 0;
+	std::size_t index = 0;
+	while (frequencyReached.find(currentFrequency) == frequencyReached.end())
+	{
+		frequencyReached.insert(currentFrequency);
+		currentFrequency += data[index++ % data.size()];
+	}
+
+	// Part 2
+	ASSERT_EQ(83445, currentFrequency);
 }
